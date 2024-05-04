dcb4dce21b6cb3de9ce074dc7f98b9c8350d5031
jatin <jatindumka@gmail.com>
2024-05-04 18:38:36 +0530

    labour cost calculator page initial setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [crewCatalog, setCrewCatalog] = useState([
+    { id: 1, type: "Foreman", pricePerUnit: 50 },
+    { id: 2, type: "Operator", pricePerUnit: 40 },
+  ]);
+
+  const addJob = (newJob) => {
+    setJobs([...jobs, newJob]);
+  };
+
   return (
-    <div className="App">
-     
+    <div>
+      <h1>Construction Labor Cost Calculator</h1>
+      <JobForm addJob={addJob} crewCatalog={crewCatalog} />
+      <CrewCatalog crewCatalog={crewCatalog} />
+      <JobList jobs={jobs} />
     </div>
   );
-}
+};
 
 export default App;
